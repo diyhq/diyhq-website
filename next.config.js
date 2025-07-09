157588c2e8d9aa79f14f68daa88311f86a33d62a
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-09 17:13:04 -0500

    Fix: switch to SSR build and remove static export

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.sanity.io']
+  }
+};
+
+module.exports = nextConfig;
