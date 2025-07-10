1d742887a01886e69546671597b6dfe1ce6b7559
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-10 17:59:32 -0500

    Fix sitemap API route handler

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,15 +1,13 @@
-export default async function handler(req, res) {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+// pages/api/sitemap.js
+
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Cache-Control", "no-store");
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://diyhq.vercel.app/</loc>
-    <changefreq>monthly</changefreq>
     <priority>1.0</priority>
   </url>
-</urlset>`;
-
-  res.setHeader("Content-Type", "application/xml");
-  res.setHeader("Cache-Control", "no-store");
-  res.write(sitemap);
-  res.end();
+</urlset>`);
 }
