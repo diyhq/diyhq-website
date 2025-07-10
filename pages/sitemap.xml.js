864a76f3a0eb2d64b45d35f81c8e511daf1e7673
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-10 17:42:29 -0500

    Fix: server-rendered sitemap.xml using getServerSideProps

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,13 +1,17 @@
-// pages/sitemap.xml.js
 export const getServerSideProps = async ({ res }) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://diyhq.vercel.app/</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
+  <url>
+    <loc>https://diyhq.vercel.app/about</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <!-- Add more URLs here -->
 </urlset>`;
 
   res.setHeader('Content-Type', 'application/xml');
