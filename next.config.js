3ae4526d21690dda572825f6a0341dda92b60c5a
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-10 16:11:43 -0500

    Move sitemap to pages/sitemap.xml.js for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io']
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap'
-      }
-    ];
   }
 };
 
