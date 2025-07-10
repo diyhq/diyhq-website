930e9effeecb4dfeeeeb54d1ec4072474ef041c0
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-10 16:43:34 -0500

    Force sitemap rewrite redeploy

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.sanity.io']
+  },
   async rewrites() {
     return [
       {
         source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
+        destination: '/api/sitemap' // this rewrite triggers sitemap.js
+      }
     ];
-  },
+  }
 };
 
+// Dummy comment to force redeploy
 module.exports = nextConfig;
