d08daf19f74a8ce675a0b20b842f9442d84ea74e
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-10 16:46:33 -0500

    Moved sitemap to /pages/sitemap.xml.js for SEO and indexing

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io']
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap' // this rewrite triggers sitemap.js
-      }
-    ];
   }
 };
 
-// Dummy comment to force redeploy
 module.exports = nextConfig;
