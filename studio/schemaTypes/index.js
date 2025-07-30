6720e050ba097d603a1a7da1481920f0a6c698e5
Topher Hedrick <diyheadquaters@gmail.com>
2025-07-30 00:39:30 -0400

    Add blockContent schema and register in index.js

--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -1,4 +1,7 @@
+// /studio/schemaTypes/index.js
+
 import post from './post'
 import category from './category'
+import blockContent from './blockContent'  // ✅ Add this line
 
-export const schemaTypes = [post, category]
+export const schemaTypes = [post, category, blockContent]  // ✅ Include here
